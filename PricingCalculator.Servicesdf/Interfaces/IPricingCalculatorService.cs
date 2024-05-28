4c9f61f317231be64021583dcafc0bb704ca58a4
Jaime Riffo Johansson <jaime.johansson@spoton.se>
2024-05-28 23:58:47 +0200

    Removal of old parts of the project

--- PricingCalculator.Servicesdf/Interfaces/IPricingCalculatorService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PricingCalculator.Service.Interfaces
-{
-    public interface IPricingCalculatorService
-    {
-        decimal CalculatePricing(string serviceName, int customerId, DateTime startDate, DateTime? endDate);
-        int GetAmountOfWorkingDays(DateTime startDate, DateTime endDate, bool addFreeDays, int freeDays, List<int> workingDays);
-    }
-}
