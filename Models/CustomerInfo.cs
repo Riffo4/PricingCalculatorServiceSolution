84b9c48192490a64d7f33193291b4c9e205b8cdb
Jaime Riffo Johansson <jaime.johansson@spoton.se>
2024-05-29 00:00:16 +0200

    Removal of more old parts of the project

--- Models/CustomerInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Models
-{
-    public class CustomerInfo
-    {
-        public Dictionary<string, PricingServiceModel> PricingServices { get; set; }
-        public int FreeDays { get; set; }
-        public string Name { get; set; }
-    }
-}
